ec09525cd696220314817ebbd3b164e20879ee13
james fowler <jamesfowler1000@gmail.com>
2016-12-08 21:30:38 -0800

    adding styles and hard coding

--- dom.js
+++ dom.js
@@ -3,23 +3,22 @@ console.log('hello world')
 
 var button = document.querySelector('button');
 
-button.addEventListener('click', function() {
-  var newNote = document.createElement('div');
-  var newSpan = document.createElement('span');
-  var newP = document.createElement('p')
-  newSpan.innerHTML = 'x';
-  newSpan.classList.add('clearfix')
-  newSpan.addEventListener('click', removeX);
-  newNote.classList.add('stickyNote');
-  newNote.appendChild(newSpan);
-  newNote.appendChild(newP);
-  newP.setAttribute('contenteditable', 'true');
-  console.log(newNote)
-  document.body.appendChild(newNote);
-  counter();
+button.addEventListener('click', function(event) {
+    var newNote = document.createElement('div');
+    var newSpan = document.createElement('span');
+    var newP = document.createElement('p')
+    newSpan.textContent = 'x';
+    newSpan.addEventListener('click', removeX);
+    newNote.classList.add('stickyNote');
+    newNote.appendChild(newSpan);
+    newNote.appendChild(newP);
+    newP.setAttribute('contenteditable', 'true');
+    console.log(newNote)
+    document.body.appendChild(newNote);
+    counter();
 });
 
-var  removeX = function(event) {
+var removeX = function(event) {
   this.parentNode.remove();
   counter();
 }
@@ -29,7 +28,7 @@ var counter = function() {
   count = document.querySelectorAll('.stickyNote').length;
   // console.log(document.querySelectorAll('.stickyNote').length);
   var turtles = document.getElementById('count');
-  turtles.innerHTML = count;
+  turtles.textContent = count;
 }
 
 
